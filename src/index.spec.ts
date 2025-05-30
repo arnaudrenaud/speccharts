ac10cc400f9c291cc55c4afd770235c682ff7192
Arnaud Renaud <arnaud.renaud@gmail.com>
2025-05-30 10:24:26 +0200

    WIP

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,6 +2,8 @@ function sum(a: number, b: number): number {
   return a + b;
 }
 
+sum(1, 2);
+
 describe("Sum", () => {
   it("returns the sum of two numbers", () => {
     expect(sum(1, 2)).toBe(3);
