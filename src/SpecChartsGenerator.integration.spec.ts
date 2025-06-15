95076a8ebd895c39d5984c6ecf70f6a5b104db23
Arnaud Renaud <arnaud.renaud@gmail.com>
2025-06-15 23:10:15 +0200

    Add basic integration test

---
+++ src/SpecChartsGenerator.integration.spec.ts
@@ -0,0 +1,45 @@
+import { SpecChartsGenerator } from "./SpecChartsGenerator";
+import { FileWithContent } from "./types";
+
+describe("SpecChartsGenerator (integration tests)", () => {
+  it("writes chart file based on spec file", async () => {
+    const SPEC_FILE_NAME = "math.spec.ts";
+    const SPEC_FILE_CONTENT = `
+      describe("math", () => {
+        describe("add", () => {
+          it("adds two numbers", () => {});
+        });
+      });
+    `;
+
+    const writeFileMock = jest.fn();
+    const generator = new SpecChartsGenerator(
+      {
+        specFilePatterns: [SPEC_FILE_NAME],
+      },
+      async () => [SPEC_FILE_NAME],
+      async (path) => {
+        return {
+          path,
+          content: path === SPEC_FILE_NAME ? SPEC_FILE_CONTENT : "",
+        };
+      },
+      writeFileMock
+    );
+
+    await generator.generate();
+
+    expect(writeFileMock).toHaveBeenCalledTimes(1);
+    expect(writeFileMock).toHaveBeenCalledWith({
+      path: `${SPEC_FILE_NAME}.mmd`,
+      content: `flowchart TD
+title["**src/math.spec.ts**"]
+N0(["math"])
+N1["add"]
+N0 --> N1
+N2(["adds two numbers"])
+N1 --> N2
+`,
+    } as FileWithContent);
+  });
+});
