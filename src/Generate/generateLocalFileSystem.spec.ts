2de8c96f157394b6215c766745080c4f8397687a
Arnaud Renaud <arnaud.renaud@gmail.com>
2025-07-04 08:39:31 +0200

    Test for multiple glob patterns

--- src/Generate/generateLocalFileSystem.spec.ts
+++ src/Generate/generateLocalFileSystem.spec.ts
@@ -13,21 +13,21 @@ const cleanUpLocalFileSystem = async () => {
 jest.mock("./helpers/log");
 
 describe("generateLocalFileSystem", () => {
-  const testFile1 = "index1.spec.ts";
-  const testFile2 = "index2.spec.ts";
+  const TEST_FILE_NAME_1 = "index.spec.ts";
+  const TEST_FILE_NAME_2 = "index.test.ts";
 
   beforeEach(async () => {
     await cleanUpLocalFileSystem();
 
     await fsExtra.outputFile(
-      path.join(TEST_FILES_DIRECTORY, testFile1),
-      `describe("index 1", () => {
+      path.join(TEST_FILES_DIRECTORY, TEST_FILE_NAME_1),
+      `describe("some test suite", () => {
   it("works", () => {});
 });`
     );
     await fsExtra.outputFile(
-      path.join(TEST_FILES_DIRECTORY, testFile2),
-      `describe("index 2", () => {
+      path.join(TEST_FILES_DIRECTORY, TEST_FILE_NAME_2),
+      `describe("some other test suite", () => {
   it("works too", () => {});
 });`
     );
@@ -37,7 +37,10 @@ describe("generateLocalFileSystem", () => {
 
   it("reads test files from glob pattern, writes chart files to output directory", async () => {
     await generateLocalFileSystem({
-      testFilesGlobPatterns: [`${TEST_FILES_DIRECTORY}/**/*.spec.ts`],
+      testFilesGlobPatterns: [
+        `${TEST_FILES_DIRECTORY}/**/*.spec.ts`,
+        `${TEST_FILES_DIRECTORY}/**/*.test.ts`,
+      ],
       outputDirectoryPath: OUTPUT_DIRECTORY,
     });
 
@@ -45,26 +48,26 @@ describe("generateLocalFileSystem", () => {
     expect(filesInOutputDirectory).toHaveLength(2);
 
     const chart1FileName = filesInOutputDirectory[0];
-    expect(chart1FileName).toEqual("index1.spec.ts.mmd");
+    expect(chart1FileName).toEqual(`${TEST_FILE_NAME_1}.mmd`);
 
     const chart1FileContent = (
       await fsExtra.readFile(path.join(OUTPUT_DIRECTORY, chart1FileName))
     ).toString();
     expect(chart1FileContent).toEqual(`flowchart TD
-title[\"**${TEST_FILES_DIRECTORY}/index1.spec.ts**\"]
-N0([\"index 1\"])
+title[\"**${TEST_FILES_DIRECTORY}/${TEST_FILE_NAME_1}**\"]
+N0([\"some test suite\"])
 N1([\"works\"])
 N0 --> N1`);
 
     const chart2FileName = filesInOutputDirectory[1];
-    expect(chart2FileName).toEqual("index2.spec.ts.mmd");
+    expect(chart2FileName).toEqual(`${TEST_FILE_NAME_2}.mmd`);
 
     const chart2FileContent = (
       await fsExtra.readFile(path.join(OUTPUT_DIRECTORY, chart2FileName))
     ).toString();
     expect(chart2FileContent).toEqual(`flowchart TD
-title[\"**${TEST_FILES_DIRECTORY}/index2.spec.ts**\"]
-N0([\"index 2\"])
+title[\"**${TEST_FILES_DIRECTORY}/${TEST_FILE_NAME_2}**\"]
+N0([\"some other test suite\"])
 N1([\"works too\"])
 N0 --> N1`);
   });
